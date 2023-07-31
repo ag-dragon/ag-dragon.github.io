7388ca101252b2b41772b90975febdaa2e45fa3c
ag-dragon <h28941+github@gmail.com>
2023-07-31 00:43:29 -0400

    Move post listing script to seperate file

---
+++ listPosts.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.articles');
+fetch('https://ag-dragon.github.io/postdata.json')
+    .then((res) => res.json())
+    .then((data) => {
+        data.data.forEach(post => {
+const article = document.createElement('div');
+article.classList.toggle('article');
+
+    const postDate = document.createElement('div');
+    postDate.classList.toggle('date');
+    postDate.textContent = new Date(Date.parse(post.date)).toDateString();
+    article.appendChild(postDate);
+
+    const postInfo = document.createElement('div');
+        const postTitle = document.createElement('div');
+        postTitle.classList.toggle('title');
+        postTitle.textContent = post.title;
+        postInfo.appendChild(postTitle);
+        const postDesc = document.createElement('div');
+        postDesc.classList.toggle('desc');
+        postDesc.textContent = post.description;
+        postInfo.appendChild(postDesc);
+        const tagList = document.createElement('ul');
+            post.tags.forEach(tag => {
+                const tagli = document.createElement('li');
+                tagli.textContent = tag;
+                tagList.appendChild(tagli);
+            });
+        postInfo.appendChild(tagList);
+    article.appendChild(postInfo);
+
+container.appendChild(article);
+            console.log(post);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
