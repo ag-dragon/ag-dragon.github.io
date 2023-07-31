b8da48451530b2fd1a32ffc0bd51ab0b98c6bdf2
ag-dragon <h28941+github@gmail.com>
2023-07-30 23:54:07 -0400

    Move template html to _template.html

--- _rendermd.js
+++ _rendermd.js
@@ -3,7 +3,6 @@ import parseMD from 'parse-md';
 import * as markdown from 'markdown-wasm';
 
 const filenames = fs.readdirSync('./_posts');
-console.log(filenames);
 
 filenames.forEach(file => {
     let data;
@@ -14,36 +13,21 @@ filenames.forEach(file => {
     }
 
     const { metadata, content } = parseMD(data);
-    console.log(metadata);
-    console.log(metadata.date.getFullYear());
-    console.log(metadata.date.getMonth());
-    console.log(metadata.date.getDate());
 
     const htmlContent = markdown.parse(content);
 
-    const output =
-`<!DOCTYPE html>
-
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <title>Personal Site</title>
-        <link rel="stylesheet" href="../style.css" />
-    </head>
-    <body>
-        <div class="header">
-            <div class="logo">Title</div>
-            <ul>
-                <li><a href="../index.html">Home</a></li>
-                <li><a href="../about.html">About</a></li>
-                <li><a href="../projects.html">Projects</a></li>
-            </ul>
-        </div>
-
-        ${htmlContent}
-    </body>
-</html>`;
+    let template;
+    try {
+        template = fs.readFileSync('./_template.html', 'utf8');
+    } catch (err) {
+        console.error(err);
+    }
+    const output = template.replace(/{markdown}/, htmlContent);
 
     fs.writeFile(`./posts/${metadata.title.replace(/\s+/g, '-').toLowerCase()}.html`,
-        output, (err) => { console.error(err) });
+        output, (err) => {
+            if (err) {
+                console.error(err)
+            }
+    });
 });
