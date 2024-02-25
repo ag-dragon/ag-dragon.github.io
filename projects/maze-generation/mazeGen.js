7c1afe1f4064a214557ec80d0449f7b87ad4581a
ag-dragon <h28941+github@gmail.com>
2024-02-25 13:58:08 -0500

    Draw gradient to canvas

--- projects/maze-generation/mazeGen.js
+++ projects/maze-generation/mazeGen.js
@@ -1,10 +1,16 @@
 function draw() {
     const canvas = document.getElementById("mazeCanvas");
     const ctx = canvas.getContext("2d");
+    var imageData = ctx.createImageData(canvas.width, canvas.height);
 
-    ctx.fillStyle = "rgb(200, 0, 0)";
-    ctx.fillRect(10, 10, 50, 50);
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(30, 30, 50, 50);
+    const data = imageData.data
+    for (let i = 0; i < data.length; i+= 4) {
+        data[i+0] = (((i/4)%canvas.width)/canvas.width)*255;
+        data[i+1] = (((i/4)/canvas.width)/canvas.height)*255;
+        data[i+2] = 255;
+        data[i+3] = 255;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
 }
 window.addEventListener("load", draw);
